1e4494d726ea7466908a51ee9cacda54f3b32d5b
Sharad_20 <83809278+sharadkumar485@users.noreply.github.com>
2023-05-17 20:49:42 -0700

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 # import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+pipe = pickle.load(open("pipe.pkl","rb"))
+df = pickle.load(open("df.pkl","rb"))
 
 st.title("Laptop Predictor")
 
